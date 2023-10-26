90461763973b38eedb35d4187a74d2d997a0adf8
nagarcia94 <000nagarcia@gmail.com>
2023-10-26 17:19:17 -0300

    Create exercicio.01.php

---
+++ exercicio.01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 01</title>
+</head>
+<body>
+<?php
+    const NOME = "Nathalia";
+    const IDADE = 29;
+    ?>
+
+    <p>A Surfista <?=NOME?> de <?=IDADE?> anos foi medalhista de OURO nos jogos PAN AMERICANO 2023.</p>
+</body>
+</html>
+
+
+
+<!-- Crie um novo arquivo chamado exercicio01.php e nele crie variáveis e/ou constantes para: 
+
+Nome de uma pessoa (exemplo: Chapolin)
+Idade que esta pessoa tem (exemplo: 25)
+Mostre no HTML uma mensagem qualquer apresentando o nome da pessoa e a idade dela. Exemplo: "Fulano possui 25 anos."
+
+Destaque o nome em itálico e a idade em negrito.-->
