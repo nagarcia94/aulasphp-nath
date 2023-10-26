34c3f7cdbaa4140a96025293b9127f4612b671c5
nagarcia94 <000nagarcia@gmail.com>
2023-10-26 15:29:36 -0300

    usando php

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iniciando com PHP</title>
+</head>
+<body>
+  <h1>Experiências com PHP</h1>
+  <hr> 
+  <!-- delimitadores -->
+  <?php 
+  echo "<i>Olá, PHP!</i>";
+  echo 2023;
+  
+
+
+
+  ?>
+</body>
+</html>
