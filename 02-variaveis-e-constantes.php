5df39e112eaba109e5d658d909e83940ba8b40a2
nagarcia94 <000nagarcia@gmail.com>
2023-10-26 16:19:21 -0300

    Update 02-variaveis-e-constantes.php

--- 02-variaveis-e-constantes.php
+++ 02-variaveis-e-constantes.php
@@ -9,5 +9,22 @@
   <h1> Variáveis e Constantes - PHP</h1>
   <hr> 
 
+  <?php
+  // Variáveis 
+  $curso = "programador Web";
+  $ano = 2023;
+  $area = "Back-End";
+  
+//  Saída interpolada
+  echo "<p>Estamos no $curso em $ano</p>";
+  
+  // Não dá certo, as variavéis são tratadas como texto
+  echo '<p>Estamos no $curso em $ano</p>';
+  
+  // Saída com CONCATENAÇÃO
+  echo '<p>Estamos no ' .$curso. ' em ' .$ano.'</p>';
+  echo "<p>Estamos no " .$curso. " em " .$ano. "</p>";
+  ?>
+
 </body>
 </html>
