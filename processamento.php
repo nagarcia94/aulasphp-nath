2cf97f7801318321402b10f91e1f006e8608f7e0
nagarcia94 <000nagarcia@gmail.com>
2023-11-08 16:21:06 -0300

    Update processamento.php

--- processamento.php
+++ processamento.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Processando os dados</title>
 </head>
+
 <body>
     <h1>Processamento dos dados vindo do formulário</h1>
     <hr>
@@ -15,28 +17,29 @@
     $email = $_POST["email"];
     $nascimento = $_POST["nascimento"];
     $mensagem = $_POST["mensagem"];
-    
-    if( empty($nome) || empty ($email)){
-        ?> 
-        <p> Preencha os campos vazios!! </p>
-        <?php
 
-    }else {
+    if (empty($nome) || empty($email)) {
     ?>
+        <p> Preencha os campos vazios!! </p>
+    <?php
 
-   <h2>Dados recebidos:</h2>
-<ul>
-    <li>Nome: <?=$nome?></li>
-    <li>Email <?=$email?></li>
-    <li>Data de Nascimento: <?=$nascimento?></li>
-    <li>Mensagem: <?=$mensagem?></li>
-</ul>
-<?php } ?>
+    } else { ?>
+        <h2>Dados recebidos:</h2>
+        <ul>
+            <li>Nome: <?= $nome ?></li>
+            <li>Email <?= $email ?></li>
+
+            <?php if (!empty($nascimento)) { ?>
+                <li>Data de Nascimento: <?= $nascimento ?></li>
+            <?php } ?>
 
 
-
-
-
+            <?php if (!empty($mensagem)) { ?>
+                <li>Mensagem: <?= $mensagem ?></li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
 
 </body>
+
 </html>
